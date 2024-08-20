86bd75762404c085bc79aea7514ac960bcf48908
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-08-20 23:58:46 +0100

    Update navbar.js

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,14 +4,11 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
+		 <div className="container-fluid justify-content-end mx-4">
+		   <Link to="/add className="text-decoration-none">	
+		     <button className="btn btn-primary">Add new contact</button>
+	           </Link>
+		 </div>
 		</nav>
-	);
+   );
 };
