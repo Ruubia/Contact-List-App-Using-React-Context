0872cec8f2ade2f3cae500844e5d910d9e35499e
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-08-21 19:48:15 +0100

    Update navbar.js

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 		 <div className="container-fluid justify-content-end mx-4">
-		   <Link to="/add className="text-decoration-none">	
+		   <Link to="/add" className="text-decoration-none">	
 		     <button className="btn btn-primary">Add new contact</button>
 	           </Link>
 		 </div>
