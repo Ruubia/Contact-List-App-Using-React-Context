5ecdc366a621f7335f04325812d357424053f4da
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-08-21 19:43:53 +0100

    Update addContact.js

--- src/js/views/addContact.js
+++ src/js/views/addContact.js
@@ -1,43 +1,82 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState, useContext, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-import "../../styles/demo.css";
 
-export const Demo = () => {
-	const { store, actions } = useContext(Context);
-
-	return (
-		<div className="container">
-			<ul className="list-group">
-				{store.demo.map((item, index) => {
-					return (
-						<li
-							key={index}
-							className="list-group-item d-flex justify-content-between"
-							style={{ background: item.background }}>
-							<Link to={"/single/" + index}>
-								<span>Link to: {item.title}</span>
-							</Link>
-							{// Conditional render example
-							// Check to see if the background is orange, if so, display the message
-							item.background === "orange" ? (
-								<p style={{ color: item.initial }}>
-									Check store/flux.js scroll to the actions to see the code
-								</p>
-							) : null}
-							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
-								Change Color
-							</button>
-						</li>
-					);
-				})}
-			</ul>
-			<br />
-			<Link to="/">
-				<button className="btn btn-primary">Back home</button>
-			</Link>
-		</div>
-	);
+export const addContac = () => {
+	const { store, actions } = useContext (Context);
+	const [ contact, setContact] = useState ({
+	     name: "",
+             email: "",
+             phone: "",
+             address: ""
+	});
+	const navigate = useNavigate();
+    	const { id } = useParams();
+	
+	useEffect(() => {
+	    if (id) {
+		const selectedContact = store.contacts.find(contact => contact.id === parseIn(id));
+		if (selectedContact) {
+		   setContact({
+		      name: selectedContact.name,
+                      email: selectedContact.email,
+                      phone: selectedContact.phone,
+                      address: selectedContact.address
+		   });
+		}
+	    }
+	}, [id, store.contacts]);
+	
+	const handleChange = (e) => {
+		setContact({ ...contact, [e.target.name]: e.target.value });
+	};
+			
+	const handleSubmit = (e) => {
+	 e.preventDefault();
+         if (id) {
+            actions.updateContact(parseInt(id), contact)
+                .then(() => {
+                    navigate("/");
+                })
+                .catch(error => {
+                    console.error("Error updating contact:", error);
+                });
+          } else {
+            actions.createContact(contact)
+                .then(() => {
+                    navigate("/");
+                })
+                .catch(error => {
+                    console.error("Error creating contact:", error);
+                });
+       	 }
+    };
+    return (
+        <div className="container">
+            <div>
+                <h1 className="text-center mt-5">{id ? "Edit Contact" : "Add a new contact"}</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>Full Name</label>
+                        <input type="text" className="form-control" placeholder="Full Name" name="name" value={contact.name} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="form-control" placeholder="Enter email" name="email" value={contact.email} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Phone</label>
+                        <input type="phone" className="form-control" placeholder="Enter phone" name="phone" value={contact.phone} onChange={handleChange} />
+                    </div>
+                    <div className="form-group">
+                        <label>Address</label>
+                        <input type="text" className="form-control" placeholder="Enter address" name="address" value={contact.address} onChange={handleChange} />
+                    </div>
+                    <button type="submit" className="btn btn-primary form-control">{id ? "Update" : "Save"}</button>
+                    <Link className="mt-3 w-100 text-center" to="/">or get back to contacts</Link>
+                </form>
+            </div>
+        </div>
+    );
 };
